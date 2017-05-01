8808b69516847ca20bc951885f54ef3563f55280
vtretyakov <vtetyakov@synapticon.com>
2017-05-01 16:57:34 +0200

    fix implementation errors

--- clone.py
+++ clone.py
@@ -32,12 +32,12 @@ for line in lines:
 augmented_images, augmented_measurements = [], []
 for image, measurement in zip(images, measurements):
     augmented_images.append(image)
-    augmented_measurements.append(measurements)
+    augmented_measurements.append(measurement)
     augmented_images.append(cv2.flip(image,1))
     augmented_measurements.append(measurement*-1.0)
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+X_train = np.array(augmented_images)
+y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential, Model
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
