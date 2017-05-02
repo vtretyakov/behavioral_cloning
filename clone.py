f525cb565425a070ea24a051f2722cba579c5409
vtretyakov <vtetyakov@synapticon.com>
2017-05-02 05:09:08 +0200

    add dropout that doesn't fail

--- clone.py
+++ clone.py
@@ -40,7 +40,7 @@ X_train = np.array(augmented_images)
 y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 import matplotlib.pyplot as plt
@@ -49,7 +49,6 @@ model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation="relu"))
-model.add(Dropout(0.5))
 model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation="relu"))
 model.add(Dropout(0.5))
 model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation="relu"))
