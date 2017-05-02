7539a5a9fdd260b400248397b6f33a6b8a5dee7f
vtretyakov <vtetyakov@synapticon.com>
2017-05-02 05:07:20 +0200

    add dropout that doesn't fail

--- clone.py
+++ clone.py
@@ -5,7 +5,7 @@ import sklearn
 
 #stearing adjustments
 correction = 0.2
-epochs = 3
+epochs = 10
 
 samples = []
 with open ('training/driving_log.csv') as csvfile:
@@ -85,7 +85,7 @@ validation_generator = generator(validation_samples, batch_size=32)
 #y_train = np.array(augmented_measurements)
 
 from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Dropout
 from keras.layers import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 import matplotlib.pyplot as plt
@@ -95,12 +95,18 @@ model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 model.add(Convolution2D(24, 5, 5, subsample=(2,2), activation="relu"))
 model.add(Convolution2D(36, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Dropout(0.5))
 model.add(Convolution2D(48, 5, 5, subsample=(2,2), activation="relu"))
+model.add(Dropout(0.5))
 model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Dropout(0.5))
 model.add(Convolution2D(64, 3, 3, activation="relu"))
+model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Dropout(0.5))
 model.add(Dense(50))
+model.add(Dropout(0.5))
 model.add(Dense(10))
 model.add(Dense(1))
 
@@ -114,7 +120,7 @@ history_object = model.fit_generator(train_generator, samples_per_epoch =
                                      len(train_samples), validation_data =
                                      validation_generator,
                                      nb_val_samples = len(validation_samples),
-                                     nb_epoch=epochs, verbose=1)
+                                     epochs=epochs, verbose=1)
 
 model.save('model.h5')
 
